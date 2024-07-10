f04cae6273b5a8d4df2d8bab7484d7edce5a2478
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-07-10 13:58:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import pyrogram, os, asyncio
 
-try: app_id = int(os.environ.get("app_id", None))
+try: app_id = int(os.environ.get("app_id", "20389440"))
 except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
-try: api_hash = os.environ.get("api_hash", None)
+try: api_hash = os.environ.get("api_hash", "a1a06a18eb9153e9dbd447cfd5da2457")
 except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
-try: bot_token = os.environ.get("bot_token", None)
+try: bot_token = os.environ.get("bot_token", "6564513574:AAH3Y97iqQjSlV5vKKZdGDUohlhpA-LeSbw")
 except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
-try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
+try: custom_caption = os.environ.get("custom_caption", "`{file_name}`\n➳⃝⃝💀 @deathking_botworld\n➳⃝⃝☠️ @death_movie\n➳⃝⃝🥀 @TryToLiveAlon")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
-AutoCaptionBot = pyrogram.Client(
-   name="AutoCaptionBot", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
+AutoCaptionBotV1 = pyrogram.Client(
+   name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
 
 start_message = """
 <b>👋Hello {}</b>
 <b>I am an AutoCaption bot</b>
 <b>All you have to do is add me to your channel and I will show you my power</b>
-<b>@Mo_Tech_YT</b>"""
+<b>@TryToLiveAlon</b>"""
 
 about_message = """
-<b>• Name : [AutoCaption V1](t.me/{username})</b>
-<b>• Developer : [Muhammed](https://github.com/PR0FESS0R-99)
+<b>• Name : <a href=https://t.me/DeathAutoCaptionBot>AutoCaption</a></b>
+<b>• Developer : <a href=https://t.me/TryToLiveAlon>[DEV]</a></b>
 <b>• Language : Python3</b>
 <b>• Library : Pyrogram v{version}</b>
-<b>• Updates : <a href=https://t.me/Mo_Tech_YT>Click Here</a></b>
-<b>• Source Code : <a href=https://github.com/PR0FESS0R-99/AutoCaption-Bot>Click Here</a></b>"""
+<b>• Updates : <a href=https://t.me/deathking_botworld>Click Here</a></b>"""
 
-@AutoCaptionBot.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
+@AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
 def start_command(bot, update):
   update.reply(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
-@AutoCaptionBot.on_callback_query(pyrogram.filters.regex("start"))
+@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("start"))
 def strat_callback(bot, update):
   update.message.edit(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
-@AutoCaptionBot.on_callback_query(pyrogram.filters.regex("about"))
+@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("about"))
 def about_callback(bot, update): 
   bot = bot.get_me()
   update.message.edit(about_message.format(version=pyrogram.__version__, username=bot.mention), reply_markup=about_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
-@AutoCaptionBot.on_message(pyrogram.filters.channel)
+@AutoCaptionBotV1.on_message(pyrogram.filters.channel)
 def edit_caption(bot, update: pyrogram.types.Message):
-  if os.environ.get("custom_caption"):
-      motech, _ = get_file_details(update)
-      try:
-          try: update.edit(custom_caption.format(file_name=motech.file_name))
-          except pyrogram.errors.FloodWait as FloodWait:
-              asyncio.sleep(FloodWait.value)
-              update.edit(custom_caption.format(file_name=motech.file_name, mote))
-      except pyrogram.errors.MessageNotModified: pass 
-  else:
-      return
+  motech, _ = get_file_details(update)
+  try:
+      try: update.edit(custom_caption.format(file_name=motech.file_name))
+      except pyrogram.errors.FloodWait as FloodWait:
+          asyncio.sleep(FloodWait.value)
+          update.edit(custom_caption.format(file_name=motech.file_name))
+  except pyrogram.errors.MessageNotModified: pass 
     
 def get_file_details(update: pyrogram.types.Message):
   if update.media:
@@ -76,7 +76,7 @@ def get_file_details(update: pyrogram.types.Message):
 def start_buttons(bot, update):
   bot = bot.get_me()
   buttons = [[
-   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/Mo_Tech_YT"),
+   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/deathking_botworld"),
    pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
    ],[
    pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
@@ -90,6 +90,10 @@ def about_buttons(bot, update):
   return pyrogram.types.InlineKeyboardMarkup(buttons)
 
 print("Telegram AutoCaption V1 Bot Start")
-print("Bot Created By https://github.com/PR0FESS0R-99")
+print("Bot Created By https://t.me/deathking_botworld")
 
-AutoCaptionBot.run()
+AutoCaptionBotV1.run()
+
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
